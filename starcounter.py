0347bb6b5dda4010606e264d486c9841c600a88f
reapersy <reapersoy@proton.me>
2021-11-25 22:17:21 +0000

    Revert "Grants system server access to /proc/<pid>/oom_adj for Android applications."

--- starcounter.py
+++ starcounter.py
@@ -11,4 +11,8 @@ CACHE_DIR = 'cache_stars_json'
 os.makedirs(CACHE_DIR, exist_ok=True)
 
 def load_known_users(filename="known-users.yml"):
-	with open(filename, 'r
+	with open(filename, 'rt') as f:
+		known_users = yaml.safe_load(f)
+	return known_users
+
+def cache_know
