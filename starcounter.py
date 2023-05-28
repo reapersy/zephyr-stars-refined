ad7ec2d94086e311624a66e6bbe484c49e85b82a
reapersy <reapersoy@proton.me>
2023-05-28 09:40:30 +0000

    - updates

--- starcounter.py
+++ starcounter.py
@@ -108,4 +108,6 @@ HOWTO = """
 
 if __name__ == '__main__':
 	known_users = load_known_users()
-	cache_known_users(known_users
+	cache_known_users(known_users)
+	stars, descriptions = count_stars(known_users)
+	write_markdown(known_users, stars, description
